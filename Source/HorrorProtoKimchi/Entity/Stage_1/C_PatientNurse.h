40fec9fee11c7f2e5c5fa758fe7ce6695e0e0a5e
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-03 18:57:40 +0900

    metahuman하기전 커밋

---
+++ Source/HorrorProtoKimchi/Entity/Stage_1/C_PatientNurse.h
@@ -0,0 +1,62 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Entity/C_EntityBase.h"
+#include "C_PatientNurse.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class HORRORPROTOKIMCHI_API AC_PatientNurse : public AC_EntityBase
+{
+	GENERATED_BODY()
+
+public:
+	AC_PatientNurse();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+
+	virtual void Tick(float _DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ACharacter* PatientActor;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ACharacter* DocActor;
+
+	UPROPERTY()
+	class AAIController* AIController;
+
+
+	UFUNCTION(BlueprintCallable)
+	void Chase(ACharacter* _target, float _Radius, float _Distance, float _waitTime);
+
+	UFUNCTION(BlueprintCallable)
+	float CalculateDistance(ACharacter* _Target);
+
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetWaitTime(float _WaitTime);
+
+	UFUNCTION(BlueprintCallable)
+	bool WaitFunction(float _DeltaTime);
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float currentTime = 0.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float waitTime = 0.f;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	int32 sequenceIndex = 0;
+
+};

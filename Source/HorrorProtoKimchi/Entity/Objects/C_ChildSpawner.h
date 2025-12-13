5d475d795937c92627fcfb3070458f82bf1c5790
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-14 02:23:18 +0900

    어린아이스포너 수정

--- Source/HorrorProtoKimchi/Entity/Objects/C_ChildSpawner.h
+++ Source/HorrorProtoKimchi/Entity/Objects/C_ChildSpawner.h
@@ -6,6 +6,7 @@
 #include "Entity/C_ActorBase.h"
 #include "Components/AudioComponent.h"   // AudioComponent 헤더
 #include "Sound/SoundBase.h"             // Sound asset 헤더
+#include "Components/ArrowComponent.h"
 #include "C_ChildSpawner.generated.h"
 
 /**
@@ -33,6 +34,15 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Sound")
 	USoundBase* SoundCue;
 
+	// 블루프린트에서 위치를 옮길 "스폰 포인트(Arrow)"들
+// 이유: 블프에서 컴포넌트 이동이 편하고, 배열로 탐색하기 쉬움
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "SpawnPoints")
+	TArray<TObjectPtr<UArrowComponent>> SpawnArrows;
+
+	// 입력 위치에서 가장 가까운 Arrow 반환
+// 이유: 스폰 로직에서 가장 근접한 포인트를 빠르게 선택 가능
+	UFUNCTION(BlueprintCallable, Category = "SpawnPoints")
+	UArrowComponent* GetNearestArrow(const FVector& WorldLocation) const;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	bool bIsActive = false;

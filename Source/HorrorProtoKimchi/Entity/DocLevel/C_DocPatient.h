47f21c4386561d27dd8b570e4c873d798e822faf
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-26 17:01:19 +0900

    patient구현

--- Source/HorrorProtoKimchi/Entity/DocLevel/C_DocPatient.h
+++ Source/HorrorProtoKimchi/Entity/DocLevel/C_DocPatient.h
@@ -7,21 +7,42 @@
 #include "C_DocPatient.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class HORRORPROTOKIMCHI_API AC_DocPatient : public AC_EntityBase
 {
 	GENERATED_BODY()
-	
+
 public:
+
+	AC_DocPatient();
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	class UAudioComponent* SoundComponent;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UAudioComponent* HeartBeatComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	TArray<class USoundBase*> PatientSoundArray;
 
 	UFUNCTION(BlueprintCallable)
-	void OnEvent();
+	bool OnEvent(float _DeltaTime);
+
+	UFUNCTION(BlueprintCallable)
+	bool OnCurePatient(int32 _AmpleIndex);
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 CurrentEventIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CurrentTime = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CoolTime = 10.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool HaveToCure = false;
+
 };

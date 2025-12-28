c11875963097b57b686adea07643eab0db1c0eec
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-28 17:42:47 +0900

    doclevel 점스 제외 완전구현

---
+++ Source/HorrorProtoKimchi/Entity/DocLevel/C_DoctorDoorKnockObj.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Entity/DocLevel/C_DoctorDoorKnockObj.h"
+
+// Sets default values
+AC_DoctorDoorKnockObj::AC_DoctorDoorKnockObj()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AC_DoctorDoorKnockObj::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+bool AC_DoctorDoorKnockObj::CalculateCoolTime(float _DeltaTime)
+{
+	CurrentCoolTime += _DeltaTime;
+	if (CurrentCoolTime > CoolTime) {
+		CurrentCoolTime -= CoolTime;
+		return true;
+	}
+	return false;
+}
+
+
---
+++ Source/HorrorProtoKimchi/Entity/DocLevel/C_DoctorDoorKnockObj.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_DoctorDoorKnockObj.generated.h"
+
+UCLASS()
+class HORRORPROTOKIMCHI_API AC_DoctorDoorKnockObj : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_DoctorDoorKnockObj();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CurrentCoolTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float CoolTime;
+
+	UFUNCTION(BlueprintCallable)
+	bool CalculateCoolTime(float _DeltaTime);
+};

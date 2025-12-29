55b22ef096ec5a3c2e7bb9f30f773b4d6ff0aa76
garamzui <forsave121@gmail.com>
2025-12-30 03:13:13 +0900

    임규민 똥

--- Source/HorrorProtoKimchi/Anim/C_AnimBPBase.cpp
+++ Source/HorrorProtoKimchi/Anim/C_AnimBPBase.cpp
@@ -18,14 +18,15 @@ void UC_AnimBPBase::SetFacePose(C_FacePoseEnum pose, C_FaceMoodEnum mood, float 
 	case C_FacePoseEnum::MadLaugh :
 		FirstIndex = 6;
 		LastIndex = 6;
-	
+		break;
 	case C_FacePoseEnum::AngerYelling :
 		FirstIndex = 7;
 		LastIndex = 7;
-
+		break;
 	case C_FacePoseEnum::CloseEye :
 		FirstIndex = 8;
 		LastIndex = 8;
+		break;
 	default:
 		break;
 	}

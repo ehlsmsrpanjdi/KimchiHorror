4c3c5f30f1a00e68508fbed69a339c1135b299dd
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-29 16:47:18 +0900

    버튼패드 생성

---
+++ Source/HorrorProtoKimchi/Entity/PuzzleLevel/C_NumberPad.cpp
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Entity/PuzzleLevel/C_NumberPad.h"
+#include "Components/ArrowComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AC_NumberPad::AC_NumberPad()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("MainBody");
+
+	DirectionComponent = CreateDefaultSubobject<UArrowComponent>("DirectionComponent");
+	DirectionComponent->SetupAttachment(RootComponent);
+}
+
+void AC_NumberPad::CalculateDirection()
+{
+
+	FVector ForwardVec = DirectionComponent->GetForwardVector();
+
+	FowardDirection = ForwardVec.GetSafeNormal();
+}
+
+FVector AC_NumberPad::CalculateCurrentPos(float _Scale)
+{
+	FVector ComponentLocation = DirectionComponent->GetComponentLocation();
+	FVector Offset = FowardDirection * NumberArray.Num() * _Scale;
+	FVector value = ComponentLocation + Offset;
+	return value;
+}
+
+void AC_NumberPad::ClearNumber()
+{
+	for (AActor* act : NumberArray) {
+		if (act && IsValid(act)) {
+			act->Destroy();
+		}
+	}
+
+	NumberArray.Empty();
+}
+
---
+++ Source/HorrorProtoKimchi/Entity/PuzzleLevel/C_NumberPad.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_NumberPad.generated.h"
+
+UCLASS()
+class HORRORPROTOKIMCHI_API AC_NumberPad : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AC_NumberPad();
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	class UArrowComponent* DirectionComponent;
+
+	UFUNCTION(BlueprintCallable)
+	void CalculateDirection();
+
+	FVector FowardDirection;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<AActor*> NumberArray;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<AActor>> NumberAssetArray;
+
+	UFUNCTION(BlueprintCallable)
+	FVector CalculateCurrentPos(float _Scale = 1.f);
+
+	UFUNCTION(BlueprintCallable)
+	void ClearNumber();
+};

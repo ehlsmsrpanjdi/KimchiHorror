2d9ea6a22f5063cc617686d16a1b02289e69dea4
ehlsmsrpanjdi <usm9401@gmail.com>
2026-01-01 20:53:05 +0900

    아이엠규민 come이이잇

---
+++ Source/HorrorProtoKimchi/Sequence/C_SequenceHelper.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Sequence/C_SequenceHelper.h"
+
+// Sets default values
+AC_SequenceHelper::AC_SequenceHelper()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AC_SequenceHelper::ExecuteCallback()
+{
+	OnSequenceFinished.Broadcast();
+}
---
+++ Source/HorrorProtoKimchi/Sequence/C_SequenceHelper.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "C_SequenceHelper.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnSequenceCallback);
+
+UCLASS()
+class HORRORPROTOKIMCHI_API AC_SequenceHelper : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AC_SequenceHelper();
+
+
+
+	// 2. 블루프린트에서 함수 포인터처럼 쓸 변수
+	UPROPERTY(BlueprintAssignable, Category = "Sequence")
+	FOnSequenceCallback OnSequenceFinished;
+
+	// 3. 실행 함수
+	UFUNCTION(BlueprintCallable, Category = "Sequence")
+	void ExecuteCallback();
+};

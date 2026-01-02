6e24e275e8925263444a1c98a4f294ee628e3e2c
garamzui <forsave121@gmail.com>
2026-01-03 03:32:56 +0900

    까득뼈는 까득까득

--- Source/HorrorProtoKimchi/UI/Inventory/C_StashUI.cpp
+++ Source/HorrorProtoKimchi/UI/Inventory/C_StashUI.cpp
@@ -3,3 +3,40 @@
 
 #include "UI/Inventory/C_StashUI.h"
 
+
+
+void UC_StashUI::KHAddItem(UC_ItemData* ItemData)
+{
+	if (!ItemData)
+	{
+		return;
+	}
+
+	KHItems.Add(ItemData);
+}
+
+UC_ItemData* UC_StashUI::KHGetItem(int32 Index) const
+{
+	if (!KHItems.IsValidIndex(Index))
+	{
+		return nullptr;
+	}
+
+	return KHItems[Index].Get();
+}
+
+void UC_StashUI::KHSortItems()
+{
+	TArray<TObjectPtr<UC_ItemData>> NewItems;
+	NewItems.Reserve(KHItems.Num());
+
+	for (const TObjectPtr<UC_ItemData>& Item : KHItems)
+	{
+		if (Item)
+		{
+			NewItems.Add(Item);
+		}
+	}
+
+	KHItems = MoveTemp(NewItems);
+}
--- Source/HorrorProtoKimchi/UI/Inventory/C_StashUI.h
+++ Source/HorrorProtoKimchi/UI/Inventory/C_StashUI.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Item/C_ItemData.h"
 #include "Blueprint/UserWidget.h"
 #include "C_StashUI.generated.h"
 
@@ -13,5 +14,17 @@ UCLASS()
 class HORRORPROTOKIMCHI_API UC_StashUI : public UUserWidget
 {
 	GENERATED_BODY()
-	
+public:
+	UPROPERTY(BlueprintReadOnly, Category = "Stash")
+	TArray<TObjectPtr<UC_ItemData>> KHItems;
+
+	UFUNCTION(BlueprintCallable, Category = "Stash")
+	void KHAddItem(UC_ItemData* ItemData);
+
+	UFUNCTION(BlueprintCallable, Category = "Stash")
+	UC_ItemData* KHGetItem(int32 Index) const;
+
+	UFUNCTION(BlueprintCallable, Category = "Stash")
+	void KHSortItems();
+
 };

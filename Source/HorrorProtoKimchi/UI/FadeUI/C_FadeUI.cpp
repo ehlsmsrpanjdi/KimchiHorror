3334e2022965d12d76f4753d3e6127f2531d4572
garamzui <forsave121@gmail.com>
2026-01-25 23:08:11 +0900

    으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄으으음그믄

---
+++ Source/HorrorProtoKimchi/UI/FadeUI/C_FadeUI.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/FadeUI/C_FadeUI.h"
+
---
+++ Source/HorrorProtoKimchi/UI/FadeUI/C_FadeUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "C_FadeUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORRORPROTOKIMCHI_API UC_FadeUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
